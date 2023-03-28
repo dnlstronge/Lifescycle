f6dbec6356eedcf3c7ff14d03c5ba448bf28dbef
Dan Stronge <dnlstronge@gmail.com>
2023-03-28 10:22:49 +0100

    [x] - create new component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React, {useState} from 'react';
-import Button from "./components/UI/Button/Button"
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
 
 /* [1] - Create a button which adds a paragraph below the h1 tag 
    [2] - add a console.log to demonstrate the functional firing */
 
-
-
 // styles
-import './App.css';
-
+import "./App.css";
 
 function App() {
-  console.log("app running") // logs each time the button is clicked because state has changed and function reexecuted
-  const [addPara, setAddPara] = useState(false)
+  console.log("app running"); // logs each time the button is clicked because state has changed and function reexecuted
+  const [addPara, setAddPara] = useState(false);
   const handleButton = () => {
-    setAddPara(prev => !prev) // short version setting to opposite of previous state
-  }
+    setAddPara((prev) => !prev); // short version setting to opposite of previous state
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {addPara && 
-      <p>Added Paragraph to demonstrate how react and dom updating works</p>}
+      {addPara && (
+        <p>Added Paragraph to demonstrate how react and dom updating works</p>
+      )}
       <Button onClick={handleButton}>Add Paragraph</Button>
     </div>
-    
   );
 }
 
