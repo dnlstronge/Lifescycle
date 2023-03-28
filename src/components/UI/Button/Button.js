e2d47beabadbd7e8583ed8e2d4719473439c9b74
Dan Stronge <dnlstronge@gmail.com>
2023-03-28 12:16:56 +0100

    [x]  - add comments

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,6 +3,7 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const Button = (props) => {
+  console.log("button running")
   return (
     <button
       type={props.type || 'button'}
@@ -16,3 +17,12 @@ const Button = (props) => {
 };
 
 export default React.memo(Button);
+
+
+/* the log still fires has the button changed? 
+
+This is a common GOTCHA with react
+
+it's still called by react and so executes like a normal function (the handler 
+  essentially is recreated each time, it is therefore "new", and therefore there is a 
+  change happening, tangible or not*/
