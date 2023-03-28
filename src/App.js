626b73280add78558bd313c98437b41f00ec7e9b
Dan Stronge <dnlstronge@gmail.com>
2023-03-28 10:14:41 +0100

    [x] - add log and comment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
 import Button from "./components/UI/Button/Button"
 
-/* [1] - Create a button which adds a paragraph below the h1 tag */
+/* [1] - Create a button which adds a paragraph below the h1 tag 
+   [2] - add a console.log to demonstrate the functional firing */
+
 
 
 // styles
@@ -9,6 +11,7 @@ import './App.css';
 
 
 function App() {
+  console.log("app running") // logs each time the button is clicked because state has changed and function reexecuted
   const [addPara, setAddPara] = useState(false)
   const handleButton = () => {
     setAddPara(prev => !prev) // short version setting to opposite of previous state
