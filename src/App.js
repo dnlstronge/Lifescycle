17a3fea6a8a3a4fd23b18b86a9f620383a9fd46d
Dan Stronge <dnlstronge@gmail.com>
2023-03-28 09:51:28 +0100

    [x] - add button to display paragraph

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
+/* [1] - Create a button which adds a paragraph below the h1 tag */
 
+
+// styles
 import './App.css';
 
+
 function App() {
+  const [addPara, setAddPara] = useState(false)
+  const handleButton = () => {
+    setAddPara(addPara ? false : true)
+  }
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {addPara && 
+      <p>Added Paragraph to demonstrate how react and dom updating works</p>}
+      <button onClick={handleButton}>Add Paragraph</button>
     </div>
+    
   );
 }
 
