b7b26ee07d13a1c9489781ba9e74b7dec60005e7
Dan Stronge <dnlstronge@gmail.com>
2023-03-31 09:39:40 +0100

    [x] - add button which sets disabled for other  button

--- src/App.js
+++ src/App.js
@@ -12,16 +12,20 @@ function App() {
   console.log("app running"); 
 
   const [addPara, setAddPara] = useState(false);
-
+  const [toggleActive, setToggleActive] = useState(false)
+  const handleAllow = () => {
+    setToggleActive(prev => !prev) 
+  }
   const handleButton = useCallback(() => {
     setAddPara((prev) => !prev);
-  }, []) // tellsreact it has no deps so will never change
+  }, []) // tells react it has no deps so will never change so samefunction object reused when app rerenders
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={false}/> 
   
-      <Button onClick={handleButton}>Add Paragraph</Button>
+      <Button disabled={!toggleActive} onClick={handleButton}>Add Paragraph</Button>
+      <Button onClick={handleAllow}>Allow Toggle</Button>
     </div>
   );
 }
